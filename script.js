82b358c53c1f949b78529f41537f1a029042a002
Leisy Sánchez <leisysado@gmail.com>
2026-04-06 14:39:14 -0500

    code refactor - keep julian design -

---
+++ script.js
@@ -0,0 +1,31 @@
+const dict = {
+    "A": ["Avión", "avion.avif"]
+};
+
+function card(letter, palabra, imagen) {
+    return `<div class="col-1">
+                <div class="flashcard-container">
+                    <div class="flashcard bg-success">
+                        <div class="flashcard-front custom-blue text-white rounded-3 shadow-sm d-flex align-items-center justify-content-center">
+                            <span class="fs-2 fw-bold">${letter}</span>
+                        </div>
+                        <div class="flashcard-back bg-white border rounded-3 shadow-sm d-flex flex-column align-items-center justify-content-center">
+                            <img src="${imagen}" alt="${palabra}" class="img-fluid p-2">
+                            <p class="small fw-bold mb-0 text-dark">${palabra}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
+
+const cardsContainer = document.getElementsByClassName("container-letters")[0];
+
+for (const letter in dict) {
+    const [palabra, imagen] = dict[letter];
+
+    const temp = document.createElement("div");
+    temp.innerHTML = card(letter, palabra, imagen);
+    
+
+    cardsContainer.appendChild(temp.firstElementChild);
+}
